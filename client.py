b331dfbaf3be3d951530363372b559a4edf9774e
ngotheodore <ngoth@oregonstate.edu>
2024-05-19 14:07:29 -0700

    Update client.py

--- client.py
+++ client.py
@@ -6,10 +6,10 @@ print("Connecting to server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5557")
 
-for request in range(10):
-    print(f"Sending request {request} …")
-    socket.send(b"Quote Request")
 
-    message = socket.recv()
-    message = message.decode()
-    print(f"Received reply {request} [ {message} ]")
+print(f"Requesting Quote…")
+socket.send(b"Quote Request")
+
+message = socket.recv()
+message = message.decode()
+print(f"Received Quote [ {message} ]")
