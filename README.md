671dec3983b37b088471fedcaf83d7bd84568df7
ngotheodore <ngoth@oregonstate.edu>
2024-05-19 15:22:16 -0700

    Updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cs361microservice
-Returns inspirational exercise quotes
+Sends a request and returns inspirational exercise quotes
+
+How to use the program:
+
+1. Execute the client program, then the server program in that order.
+2. Type in the input to execute the program.
+3. The client program will send a request to the server program for a quote.
+4. The server program will send back a a quote through a socket to the client program.
+5. The client program will receive the quote, decode it, and print the quote.
+
+Diagram:
--- client.py
+++ client.py
@@ -6,10 +6,19 @@ print("Connecting to server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5557")
 
+def main():
+    print(f"Requesting Quote…")
+    socket.send(b"Quote Request")
 
-print(f"Requesting Quote…")
-socket.send(b"Quote Request")
+    message = socket.recv()
+    message = message.decode()
+    print(f"Received Quote [ {message} ]")
 
-message = socket.recv()
-message = message.decode()
-print(f"Received Quote [ {message} ]")
+while True:
+    choice = input("Press 1 to request a quote. Press 2 to quit")
+    if choice == "1":
+        main()
+    elif choice == "2":
+        break
+    else:
+        print("Invalid input")
